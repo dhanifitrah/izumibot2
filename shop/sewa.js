9affa04c34bd6c0c6867632557ccdca7030d74e1
dhanifitrah <dhanifitrah5@gmail.com>
2022-01-23 23:31:41 +0700

    first commit

--- shop/sewa.js
+++ shop/sewa.js
@@ -3,25 +3,25 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = Rp 5.000
-⬡ 2 MINGGU = Rp 8.000
-⬡ 3 MINGGU = Rp 10.000
-⬡ 1 BULAN = Rp 15.000
-⬡ PERMANEN = Rp 100.000
-⬡ PERMANEN + USER PREM = Rp 150.000
+⬡ 1 MINGGU = Rp 15.000
+⬡ 2 MINGGU = Rp 20.000
+⬡ 3 MINGGU = Rp 29.000
+⬡ 1 BULAN = Rp 35.000
+⬡ PERMANEN = Rp 230.000
+⬡ PERMANEN + USER PREM = Rp 350.000
 
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 150K
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6287833474586
 
 
 NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Bot On 24 Jam Karna Di Run Menggunakan Server Anti Lelet
 Reset Data Setiap Jam 02:00 `
 	}
 
